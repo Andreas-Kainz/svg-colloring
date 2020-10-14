de3aeb84ebb5ce2b0774df1a05950bc0319d9c08
Andreas_K <andreas_k@abwesend.de>
2020-10-14 11:08:00 +0200

    Create svg.php

---
+++ svg.php
@@ -0,0 +1,8 @@
+<link rel='stylesheet' type='text/css' href='icons.css' media='all'>
+<img src="lc_bold.svg" width="24">
+<img src="lc_alignverticalcenter.svg" width="24">
+<br \>
+<?php
+	include('lc_bold.svg');
+	include('lc_alignverticalcenter.svg');
+?>
